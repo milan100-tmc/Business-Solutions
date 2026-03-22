969925e3e00876b88609c9cb8eb864d90223fdcc
Milan <milan@Milans-MacBook-Air.local>
2026-03-22 21:44:59 +0100

    Remove dbt files from Business-Solutions

--- models/bronze/bronze_orders.sql
+++
@@ -1,2 +0,0 @@
--- Bronze: raw data as-is
-SELECT * FROM {{ source('raw', 'raw_orders') }}
--- models/silver/silver_orders.sql
+++
@@ -1,9 +0,0 @@
--- Silver: cleaned, filtered
-SELECT
-  order_id,
-  customer_id,
-  order_date,
-  amount,
-  status
-FROM {{ ref('bronze_orders') }}
-WHERE status != 'returned'
